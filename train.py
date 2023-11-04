f4d4e0551bba137bd3b3c9c04a1db0039398e5f9
Jongyeon Yoon <90745298+yjongy97@users.noreply.github.com>
2023-11-04 18:10:49 +0900

    [Feat] Update for using additional info #11

--- train.py
+++ train.py
@@ -14,13 +14,17 @@ from tqdm import tqdm
 from log import log_results
 
 import visualization as visualize
+import bfs as bfs
 
 
 def train_function(args, DEVICE, model, loss_fn, optimizer, loader):
     total_loss = 0
     model.train()
-
-    for image, label, w_mask in tqdm(loader):
+    
+    if args.distribution_loss and args.dist_loss:
+        print("<<< Use distribution loss >>>")
+    
+    for image, label, w_mask, num_units, num_pixels in tqdm(loader):
         ## change the label to have values between [0,1] 
         ## if not, loss will have negative values
         label = torch.clamp(label, min=0.0, max=1.0)
@@ -28,6 +32,7 @@ def train_function(args, DEVICE, model, loss_fn, optimizer, loader):
         image = image.float().to(device=DEVICE)
         label = label.to(device=DEVICE)
         w_mask = w_mask.to(device=DEVICE)
+        
         predictions = model(image)
 
         if args.weight_on_border:
@@ -35,6 +40,41 @@ def train_function(args, DEVICE, model, loss_fn, optimizer, loader):
         
         # calculate log loss with pixel value
         loss = loss_fn(predictions, label)
+        
+        if args.distribution_loss and args.dist_loss:
+            pred_num = []
+            pred_pixel = []
+            predictions_binary = (predictions > args.prediction_threshold).float()
+            
+            for i in range(len(predictions_binary)):
+                pred = predictions_binary[i][0].detach().cpu().numpy()
+                pred = pred*255
+                v_img = np.zeros((args.image_resize,args.image_resize), np.uint8)
+                
+                _, units = bfs.bfs(v_img, args.image_resize, args.image_resize, pred)
+                
+                if args.pore:
+                    num, pixel = bfs.padding_pore(args, units)
+                else:
+                    num, pixel = bfs.padding_np(args, units)
+                
+                pred_num.append(num)
+                pred_pixel.append(pixel)
+            
+            pred_pixel = np.array(pred_pixel)
+            pred_pixel = pred_pixel*int((2048/args.image_resize)**2)
+            
+            num_pixels = num_pixels.numpy()
+            num_units = num_units.numpy()
+            
+            t_pixels = num_pixels.shape[0]*num_pixels.shape[1]
+            
+            num_diff = (num_units-pred_num).sum()**2/len(num_units)
+            pix_diff = (num_pixels-pred_pixel).sum()**2/t_pixels
+            
+            distribution_loss = args.weight_num*(num_diff**(1/2)) + args.weight_pixel*(pix_diff**(1/2))
+            
+            loss += distribution_loss
 
         # backward
         optimizer.zero_grad()
@@ -52,7 +92,7 @@ def validate_function(args, DEVICE, model, epoch, loader):
     dice_score = 0
 
     with torch.no_grad():
-        for idx, (image, label, _) in enumerate(tqdm(loader)):
+        for idx, (image, label, _, _, _) in enumerate(tqdm(loader)):
             label = torch.clamp(label, min=0.0, max=1.0)
 
             image = image.float().to(DEVICE)
@@ -61,7 +101,7 @@ def validate_function(args, DEVICE, model, epoch, loader):
             prediction = model(image)
 
             ## make predictions to be 0. or 1.
-            prediction_binary = (prediction > 0.5).float()
+            prediction_binary = (prediction > args.prediction_threshold).float()
             dice_score += (2 * (prediction_binary * label).sum()) / ((prediction_binary + label).sum())
 
             if epoch == 0:
@@ -69,6 +109,7 @@ def validate_function(args, DEVICE, model, epoch, loader):
                 visualize.original_image_with_label(args, image, label, idx)
             if epoch % int(args.epochs / 5) == 0 or args.epochs - 1 == epoch:
                 visualize.original_image_with_prediction(args, image, prediction_binary, idx, epoch)
+                visualize.original_image_with_prediction_color(args, image, prediction_binary, label, idx, epoch) 
 
     dice = dice_score/len(loader)
     print(f"Dice score: {dice}")
@@ -99,13 +140,15 @@ def train(
             "optimizer":  optimizer.state_dict(),
         }
 
-        print("Current loss ", loss, mean_loss)
+        print("Current loss: ", loss, mean_loss)
         if best_loss > mean_loss:
             print("=====New best model=====")
             best_loss, count = mean_loss, 0
         else:
             count += 1
-
+            if count > 5:
+                args.dist_loss = True
+                
         if args.wandb:
             log_results(
                 mean_loss, dice
